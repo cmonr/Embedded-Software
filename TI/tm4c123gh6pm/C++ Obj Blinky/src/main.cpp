3d3fe2f42926c65cf1b4b46d2b615f791b8799db
Cruz Monrreal II <Curz.Monrreal@gmail.com>
2014-04-10 22:53:55 +0000

    Cleaned out useless includes from main.cpp

--- TI/tm4c123gh6pm/C++ Obj Blinky/src/main.cpp
+++ TI/tm4c123gh6pm/C++ Obj Blinky/src/main.cpp
@@ -1,34 +1,13 @@
-#include <stdarg.h>
-#include <stdbool.h>
-#include <stdint.h>
-#include <stdio.h>
-#include <string.h>
-#include <inc/hw_memmap.h>
-#include <inc/hw_ints.h>
-#include <driverlib/gpio.h>
-#include <driverlib/uart.h>
-#include <driverlib/i2c.h>
-#include <driverlib/pwm.h>
-#include <driverlib/pin_map.h>
-#include <driverlib/interrupt.h>
-#include <driverlib/sysctl.h>
-
-
-//#include "roboticsBP.h"
 #include "pin.h"
 
 #define delay(x)      SysCtlDelay(SysCtlClockGet() / 3 * x);
-
 
 int main(void)
 {
     // Clock (80MHz)
     SysCtlClockSet(SYSCTL_SYSDIV_2_5 | SYSCTL_USE_PLL | SYSCTL_XTAL_16MHZ | SYSCTL_OSC_MAIN);
-   
 
-    // Initialize Robotics BoosterPack
-    //RoboticsBP roboticsBP = RoboticsBP();
-
+    // Setup Red Led
     Pin led = Pin(PF1);
     led.setDir(OUT);
 
