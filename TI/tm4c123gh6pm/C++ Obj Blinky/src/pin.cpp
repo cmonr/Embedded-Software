a61d01bff9d81089faef4c8a1d0c2986ce2e9038
Cruz Monrreal II <Curz.Monrreal@gmail.com>
2014-04-10 22:49:23 +0000

    Successful Blinky using new objects!

--- TI/tm4c123gh6pm/C++ Obj Blinky/src/pin.cpp
+++ TI/tm4c123gh6pm/C++ Obj Blinky/src/pin.cpp
@@ -1,66 +1,75 @@
-#include <driverlib/sysctl.h>
-#include <driverlib/gpio.h>
+//#include <stdbool.h>
+//#include <stdint.h>
+//#include <inc/hw_memmap.h>
+//#include <driverlib/sysctl.h>
+//#include <driverlib/gpio.h>
 
-#define PORT_WIDTH 8
-#define PORT_COUNT PIN_COUNT / PORT_WIDTH
+#include "pin.h"
 
-typedef enum {
-    PA0, PA1, PA2, PA3, PA4, PA5, PA6, PA7,
-    PB0, PB1, PB2, PB3, PB4, PB5, PB6, PB7,
-    PC0, PC1, PC2, PC3, PC4, PC5, PC6, PC7,
-    PD0, PD1, PD2, PD3, PD4, PD5, PD6, PD7,
-    PE0, PE1, PE2, PE3, PE4, PE5, PE6, PE7,
-    PF0, PF1, PF2, PF3, PF4, PF5, PF6, PF7,
-    PIN_COUNT
-} tPin;
-
-class Pin
+Pin::Pin(tPin pin)
 {
-    public:
-        typedef enum { HiZ, Low, High } PinDir;
-        typedef enum { OpenCollector, PushPull } PinMode;
+    // TODO: Add check for previous allocation
+    //   For now, assume user is competent
+    _pins[pin] = this;
 
-        Pin( unsigned char ): _offset(offset), _port(port);
+    // Figure out internal variables
+    offset = 1 << (pin & 0x07);
+    port = _ports[pin >> 3];
 
-        void setDir( enum PinDir );
-        void toggle( void );
-        unsigned char read( void );
+    // Default Pin Values
+    _dir = IN;
+    _outState = HIGH;   // Double check this
 
-        unsigned char &offset;
-        tPort &port;
-
-    private:
-        unsigned char _offset;
-        tPort _port;
+    // Enable Port
+    SysCtlPeripheralEnable(port.periph);
 }
 
-Pin* _pin[PIN_COUNT] = {0};
-
-
-
-typedef struct
+void Pin::setDir(PinDir dir)
 {
-    unsigned int periph;
-    unsigned int base;
-}tPort;
+    // Set direction
+    if (dir == IN)
+        GPIOPinTypeGPIOInput(port.base, offset);
+    else
+        GPIOPinTypeGPIOOutput(port.base, offset);
 
+    // Save internal variable
+    _dir = dir;
+}
 
-tPort _port[6] = 
+void Pin::set(PinOutState state)
 {
-    {SYSCTL_PERIPH_GPIOA, GPIO_PORTA_BASE},
-    {SYSCTL_PERIPH_GPIOB, GPIO_PORTB_BASE},
-    {SYSCTL_PERIPH_GPIOC, GPIO_PORTC_BASE},
-    {SYSCTL_PERIPH_GPIOD, GPIO_PORTD_BASE},
-    {SYSCTL_PERIPH_GPIOE, GPIO_PORTE_BASE},
-    {SYSCTL_PERIPH_GPIOF, GPIO_PORTF_BASE}
-};
+    // Set Pin
+    if (state == LOW)
+        GPIOPinWrite(port.base, offset, 0);
+    else
+        GPIOPinWrite(port.base, offset, offset);
+    
+    // Update internal variable
+    _outState = state;
+}
 
-/* Example use case: 
-Pin btn = Pin(PF0);  // Initialization unlucks PF0 and populates _pin[PF0] entry
-...
-btn.setDir(In);
-while(btn.read() == Pin.LOW);
-while(btn.read() == Pin.HIGH);
-// Button Pressed. Do stuff.
+void Pin::toggle()
+{
+    if (_outState == LOW)
+        set(HIGH);
+    else
+        set(LOW);
+}
 
-*/
+unsigned char Pin::read()
+{
+    // Return 1 or 0
+    return GPIOPinRead(port.base, offset) != 0 ? 1 : 0;
+}
+
+void Pin::enable()
+{
+    // This is silly, but essentially set IO again 
+    setDir(_dir);
+}
+
+void Pin::disable()
+{
+    // This is silly, but make Pin an input
+    GPIOPinTypeGPIOInput(port.base, offset);
+}
