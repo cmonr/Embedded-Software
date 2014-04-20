19b9ce12c3f96a7c726632d00f84e24cdb20d9b4
Cruz Monrreal II <Curz.Monrreal@gmail.com>
2014-04-20 21:43:50 +0000

    Migrated Embedded-Software repo to KitchenSink library repo

--- TI/tm4c1233h6pm/Robotics BoosterPack Demo Bot/inc/servo.h
+++
@@ -1,9 +0,0 @@
-void initServos( void );
-
-void setServo( unsigned char, float );
-
-void setServoLimits( unsigned char, float, float );
-
-void WTimer5AIntHandler( void );
-
-void WTimer5BIntHandler( void );
--- TI/tm4c123gh6pm/Hack TX Bot/inc/encoder.h
+++
@@ -1,9 +0,0 @@
-void initEncoders( void );
-
-void invertEncoder( unsigned char );
-
-signed long readEnc( unsigned char );
-
-void PortEIntHandler( void );
-
-void PortDIntHandler( void );
